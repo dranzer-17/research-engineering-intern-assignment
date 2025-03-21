9ff09e6368626b44c289ce2fe4f2aa1bf791915f
dranzer-17 <kavish17shah2509@gmail.com>
2025-03-21 22:35:45 +0530

    server proble fix

--- app/post/[id]/page.jsx
+++ app/post/[id]/page.jsx
@@ -112,7 +112,7 @@ export default function PostDashboard() {
 
     async function fetchCategory(postTitle) {
       try {
-        const response = await fetch("https://reddify-backend.onrender.com/predict", {
+        const response = await fetch("http://localhost:5000/predict", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ post_title: postTitle }),
@@ -132,7 +132,7 @@ export default function PostDashboard() {
 
     async function fetchSentiment(postTitle) {
       try {
-        const response = await fetch("https://reddify-backend.onrender.com/predict-sentiment", {
+        const response = await fetch("http://localhost:5000/predict-sentiment", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ post_title: postTitle }),
